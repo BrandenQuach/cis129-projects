31cc461305ed07215aa8936f714eb8c3db817cc0
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-02-17 21:58:14 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -4,29 +4,37 @@
 # Constants
 priceofcoffee = 5
 priceofmuffin = 4
+priceoftea = 3
+priceofbagel = 5
 tax = 0.06
 
 # Displays Prompts
 print ('***************************************')
-print ('My Coffee and Muffin Shop')
+print ("Branden's Cafe")
 coffeeamount = int(input('Number of coffees bought?'))
 muffinamount = int(input('Number of muffins bought?'))
+teaamount = int(input('Number of teas bought?'))
+bagelamount = int(input('Number of bagels bought?'))
 print ('***************************************')
 print ('***************************************')
 
 # Equations
 coffeecost = coffeeamount * 5
 muffincost = muffinamount * 4
-taxamount = tax * (coffeecost + muffincost)
+teacost = teaamount * 3
+bagelcost = bagelamount * 5
+taxamount = tax * (coffeecost + muffincost + teacost + bagelcost)
 
 # Displays Amounts
-print ('My Coffee and Muffin Shop Receipt')
+print ("Branden's Cafe Receipt")
 print (coffeeamount ,'Coffee at $5 each: $', coffeecost)
-print (muffinamount ,'Muffin at $4 each: $', muffin cost)
+print (muffinamount ,'Muffin at $4 each: $', muffincost)
+print (teaamount ,'Tea at $3 each: $', teacost)
+print (bagelamount ,'Bagel at $5 each: $', bagelcost)
 print ('6% tax: $', taxamount)
 
 # Calculates Total
-totalcost = coffeecost + muffincost + taxamount
+totalcost = coffeecost + muffincost + teacost + bagelcost + taxamount
 
 # Displays Total
 print ('---------')
