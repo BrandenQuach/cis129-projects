245f0e79ded2bbc05ae13847e13eca90d83e19e6
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-02-17 21:39:17 -0700

    Update cis129_lab03_coffeeShop2.py

--- cis129_lab03_coffeeShop2.py
+++ cis129_lab03_coffeeShop2.py
@@ -1,1 +1,32 @@
+# Author: Branden Quach
+# Receipt for a coffee shop simulator
 
+# Constants
+priceofcoffee = 5
+priceofmuffin = 4
+tax = 0.06
+
+# Displays Prompts
+print ('***************************************','My Coffee and Muffin Shop')
+coffeeamount = int(input('Number of coffees bought?'))
+muffinamount = int(input('Number of muffins bought?'))
+print ('***************************************', ' ', '***************************************')
+
+# Equations
+coffeecost = coffeeamount * 5
+muffincost = muffinamount * 4
+taxamount = tax * (coffeecost + muffincost)
+
+# Displays Amounts
+print ('My Coffee and Muffin Shop Receipt')
+print (coffeeamount ,'Coffee at $5 each: $', coffeecost)
+print (muffinamount ,'Muffin at $4 each: $', muffin cost)
+print ('6% tax: $', taxamount)
+
+# Calculates Total
+totalcost = coffeecost + muffincost + taxamount
+
+# Displays Total
+print ('---------')
+print ('Total: $', totalcost)
+print ('***************************************')
