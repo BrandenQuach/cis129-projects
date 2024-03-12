f064d3b794ae7436b4be86daa414080174f8127e
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-03-12 01:55:28 -0700

    Update cis129_lab05.py

--- cis129_lab05.py
+++ cis129_lab05.py
@@ -1,3 +1,16 @@
+# Lab 5: The Bottle Return Program
 # Author: Branden Quach
-# 
+# March 12, 2024
+# Bottle Return
 
+# Declare Variables
+totalBottles = 0
+counter = 1
+todayBottles = 0
+totalPayout = 0
+keepGoing = "y"
+
+# Loop to run program again
+while keepGoing = "y":
+  # Code to set values of variables
+  
