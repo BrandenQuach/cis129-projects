096b04f4a9f62aa6cf1ca3476fb2af99c682b391
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-03-23 19:04:18 -0700

    Update cis129_lab06.py

--- cis129_lab06.py
+++ cis129_lab06.py
@@ -1,1 +1,7 @@
+# Lab 6: Hot Dog Cookout Calculator
+# Author: Branden Quach
+# March 23, 2024
+# Displays hotdog amounts given by input
 
+# Ask for hotdogs needed
+totalHotdog = (int(input(f'Enter total number of hotdogs needed: ')))
