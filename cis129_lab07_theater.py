16c44ca8ada03340381877577372d386f50c029e
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-03-29 21:46:40 -0700

    Update cis129_lab07_theater.py

--- cis129_lab07_theater.py
+++ cis129_lab07_theater.py
@@ -3,9 +3,6 @@
 # March 29. 2024
 # Displays number of tickets and income sold in a theater
 
-# Libraries
-import
-
 # Main function
 def main():
     # Variables
@@ -17,7 +14,7 @@ def main():
     sectionBseats = 500
     sectionCseats = 200
     totalticketsSold = 0
-    totalincome = 0
+    totalIncome = 0
 
     while endProgram == 'no':
         # Local Variables
@@ -26,22 +23,37 @@ def main():
         seatsC = 0 # Tickets sold in section C
         ticketsSold = 0
         income = 0
-
-        ticketsSold = inputTickets(seatsA, seatsB, seatsC, ticketsSold)
-
+        # Call to calculate seats sold
+        seatsA = seatsSoldA(seatsA)
+        seatsB = seatsSoldB(seatsB)
+        seatsC = seatsSoldC(seatsC)
+        
+        ticketsSold = inputTickets(seatsA, seatsB, seatsC)
         income = price(seatsA, seatsB, seatsC, sectionA, sectionB, sectionC)
+        totalticketsSold += ticketsSold
+        totalIncome += income
+
+        sectionAseats -= seatsA
+        sectionBseats -= seatsB
+        sectionCseats -= seatsC
 
         receipt(seatsA, seatsB, seatsC, sectionA, sectionB, sectionC, ticketsSold, income)
-
+        seatsLeft(sectionAseats, sectionBseats, sectionCseats)
+        
         endProgram = input(f'Do you want to end the program? (yes/no): ')
 
-def inputTickets(seatsA, seatsB, seatsC, ticketsSold):
+def seatsSoldA(seatsA):
     seatsA = (int(input(f'Enter the number of seats sold in section A: ')))
+    return seatsA
+def seatsSoldB(seatsB):
     seatsB = (int(input(f'Enter the number of seats sold in section B: ')))
+    return seatsB
+def seatsSoldC(seatsC):
     seatsC = (int(input(f'Enter the number of seats sold in section C: ')))
-  
+    return seatsC
+def inputTickets(seatsA, seatsB, seatsC):
     ticketsSold = seatsA + seatsB + seatsC
-    return seatsA, seatsB, seatsC, ticketsSold
+    return ticketsSold
 
 def price(seatsA, seatsB, seatsC, sectionA, sectionB, sectionC):
     income = (seatsA * sectionA) + (seatsB * sectionB) + (seatsC * sectionC)
@@ -51,6 +63,16 @@ def receipt(seatsA, seatsB, seatsC, sectionA, sectionB, sectionC, ticketsSold, i
     print(seatsA, 'seats sold in section A at $', sectionA, 'per ticket')
     print(seatsB, 'seats sold in section B at $', sectionB, 'per ticket')
     print(seatsC, 'seats sold in section C at $', sectionC, 'per ticket')
+    print()
     print(f'The amount of tickets sold is: ', ticketsSold)
     print(f'The income is: ', income)
-  
+    print()
+    print(f'The total amount of tickets sold is: ', totalticketsSold)
+    print(f'The total income is: $', totalIncome)
+def seatsLeft(sectionAseats, sectionBseats, sectionCseats):
+    print()
+    print(f'The seats remaining is:')
+    print(f'Section A:', sectionAseats)
+    print(f'Section B:', sectionBseats)
+    print(f'Section C:', sectionCseats)
+main()
