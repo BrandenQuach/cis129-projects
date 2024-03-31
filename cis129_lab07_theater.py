26a252d5f929e5ed8bbf2ed4cc3fad628f253399
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-03-30 22:29:49 -0700

    Update cis129_lab07_theater.py

--- cis129_lab07_theater.py
+++ cis129_lab07_theater.py
@@ -1,12 +1,15 @@
 # Lab 7: Theater Seating
 # Author: Branden Quach
-# March 29. 2024
+# March 30. 2024
 # Displays number of tickets and income sold in a theater
+
+# Libraries
+import string
 
 # Main function
 def main():
     # Variables
-    endProgram = 'yes'
+    endProgram = 'YES'
     sectionA = 20 # Cost of one ticket in section A
     sectionB = 15 # Cost of one ticket in section B
     sectionC = 10 # Cost of one ticket in section C
@@ -15,10 +18,7 @@ def main():
     sectionCseats = 200
     totalticketsSold = 0
     totalIncome = 0
-    print(f'Section A seats:', sectionAseats, 'at $', sectionA)
-    print(f'Section B seats:', sectionBseats, 'at $', sectionB)
-    print(f'Section C seats:', sectionCseats, 'at $', sectionC)
-    while endProgram == 'yes':
+    while endProgram == 'Y' or endProgram == 'YE' or endProgram == 'YES':
         print()
         # Local Variables
         seatsA = 0 # Tickets sold in section A
@@ -26,10 +26,14 @@ def main():
         seatsC = 0 # Tickets sold in section C
         ticketsSold = 0
         income = 0
+        # Section seat amounts and costs
+        print(f'Section A seats:', sectionAseats, 'at $', sectionA)
+        print(f'Section B seats:', sectionBseats, 'at $', sectionB)
+        print(f'Section C seats:', sectionCseats, 'at $', sectionC)
         # Call to calculate seats sold
-        seatsA = seatsSoldA(seatsA)
-        seatsB = seatsSoldB(seatsB)
-        seatsC = seatsSoldC(seatsC)
+        seatsA = seatsSoldA(seatsA, sectionAseats)
+        seatsB = seatsSoldB(seatsB, sectionBseats)
+        seatsC = seatsSoldC(seatsC, sectionCseats)
         # Call to calculate tickets sold and income
         ticketsSold = inputTickets(seatsA, seatsB, seatsC)
         income = price(seatsA, seatsB, seatsC, sectionA, sectionB, sectionC)
@@ -40,25 +44,31 @@ def main():
         sectionAseats -= seatsA
         sectionBseats -= seatsB
         sectionCseats -= seatsC
-
+        # Calls receipt  and seats left function
         receipt(seatsA, seatsB, seatsC, sectionA, sectionB, sectionC, ticketsSold, income, totalticketsSold, totalIncome)
         seatsLeft(sectionAseats, sectionBseats, sectionCseats)
-
-        if sectionAseats <= 0 or sectionBseats <= 0 or sectionCseats <= 0:
-            print()
-            print(f'One or more sections are sold out')
-        
-        endProgram = input(f'Do you want to enter more data? (yes/no): ')
-# Prompts seats sold
-def seatsSoldA(seatsA):
+        # Calls to prompt a program ending
+        endProgram = upperendProgram()
+# Prompts seats sold and detects valid input
+def seatsSoldA(seatsA, sectionAseats):
     seatsA = (int(input(f'Enter the number of seats sold in section A: ')))
+    while seatsA > sectionAseats:
+        print(f'Invalid number entered')
+        seatsA = (int(input(f'Enter the number of seats sold in section A: ')))
     return seatsA
-def seatsSoldB(seatsB):
+def seatsSoldB(seatsB, sectionBseats):
     seatsB = (int(input(f'Enter the number of seats sold in section B: ')))
+    while seatsB > sectionBseats:
+        print(f'Invalid number entered')
+        seatsB = (int(input(f'Enter the number of seats sold in section B: ')))
     return seatsB
-def seatsSoldC(seatsC):
+def seatsSoldC(seatsC, sectionCseats):
     seatsC = (int(input(f'Enter the number of seats sold in section C: ')))
+    while seatsC > sectionCseats:
+        print(f'Invalid number entered')
+        seatsC = (int(input(f'Enter the number of seats sold in section C: ')))
     return seatsC
+# Calculates total tickets sold
 def inputTickets(seatsA, seatsB, seatsC):
     ticketsSold = seatsA + seatsB + seatsC
     return ticketsSold
@@ -84,5 +94,13 @@ def seatsLeft(sectionAseats, sectionBseats, sectionCseats):
     print(f'Section A:', sectionAseats)
     print(f'Section B:', sectionBseats)
     print(f'Section C:', sectionCseats)
+# Determines if entry for yes/no loop is valid
+def upperendProgram():
+    touppercase = input(f'Do you want to enter more data? (YES/NO): ')
+    endProgram = touppercase.upper()
+    if endProgram == 'Y' or endProgram == 'YE' or endProgram == 'YES' or endProgram == 'N' or endProgram == 'NO':
+        return endProgram
+    else:
+        print(f'Invalid entry')
     
 main()
