80d9e408e2bdcb01bc7561369ec0faaceb7178ea
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-04-13 21:43:50 -0700

    Update cis129_lab08.py

--- cis129_lab08.py
+++ cis129_lab08.py
@@ -3,48 +3,70 @@
 # April 13, 2024
 # Checks if input is a palindrome or not
 
+# Constant variables
 stack = []
 top = -1
+
 # Main function
 def main():
-    word = getValidWord()
-
+    # Calls function for user input
+    getWord = getValidWord()
+    # Removes spaces and lowercases input
+    lowerWord = getWord.replace(' ', '').lower()
+    # Ignores special characters
+    word = ''.join(characters for characters in lowerWord if characters.isalnum())
+    # Calls palindrome function for a true or false output
     if isPalindrome(word):
-        print(f'{word} is a palindrome!')
+        print(f'{getWord.capitalize()} is a palindrome!')
     else:
-        print(f'{word} is not a palindrome.')
-
+        print(f'{getWord.capitalize()} is not a palindrome.')
+        
+# Function to receive input from user
 def getValidWord():
+    # User prompt
     getWord = input('Enter a word: ')
-    try:
-        return str(getWord)
-    except ValueError:
-        print(f'{word} is not a valid word. Please try again.')
+    # Checks for integers within input
+    if any(characters.isdigit() for characters in getWord):
+        print(f'{getWord.capitalize()} is not a valid word. Please try again.')
         return getValidWord()
+    else:
+        return str(getWord)
+
+# Function to check if input is a palindrome
 def isPalindrome(word: str) -> bool:
-    wordRefined = word.replace(' ', '').lower()
-    wordLength = len(wordRefined)
-    stack = ['0'] * (wordLength +1)
-    mid = wordLength // 2
+    # Allows data transfer for stack variable
+    global stack
+    # Calculates word length
+    length = len(word)
+    # Creates a stack according to word length
+    stack = ['0'] * (length +1)
+    # Finds the middle of word
+    mid = length // 2
     i = 0
-    while 1 < mid:
-        push(wordRefined[i])
+    # Omits middle letter for odd number of letter words
+    while i < mid:
+        push(word[i])
         i += 1
-    if wordLength % 2 != 0:
+    if length % 2 != 0:
         i += 1
-    while i < wordLength:
-        ele = pop()
-        if ele != wordRefined[i]:
+    # Checks if letters are the same from front and back
+    while i < length:
+        character = pop()
+        if character != word[i]:
             return False
         i += 1
-    return true
+    return True
+
+# Removes character from the top of stack
 def pop():
     global top
-    ele = stack[top]
+    character = stack[top]
     top -= 1
-    return ele
-def push(ele: str):
+    return character
+# Adds character to the top of stack
+def push(character: str):
     global top
     top += 1
-    stack[top] = ele
+    stack[top] = character
+# Calls main function
 main()
