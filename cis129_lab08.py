77a061ab1f8e72eee08dc6115e94465aa233a3bc
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-04-15 12:00:29 -0700

    Update cis129_lab08.py

--- cis129_lab08.py
+++ cis129_lab08.py
@@ -17,9 +17,9 @@ def main():
     word = ''.join(characters for characters in lowerWord if characters.isalnum())
     # Calls palindrome function for a true or false output
     if isPalindrome(word):
-        print(f'{getWord.capitalize()} is a palindrome!')
+        print(f'{getWord} is a palindrome!')
     else:
-        print(f'{getWord.capitalize()} is not a palindrome.')
+        print(f'{getWord} is not a palindrome.')
         
 # Function to receive input from user
 def getValidWord():
@@ -27,7 +27,7 @@ def getValidWord():
     getWord = input('Enter a word: ')
     # Checks for integers within input
     if any(characters.isdigit() for characters in getWord):
-        print(f'{getWord.capitalize()} is not a valid word. Please try again.')
+        print(f'{getWord} is not a valid word. Please try again.')
         return getValidWord()
     else:
         return str(getWord)
@@ -37,34 +37,34 @@ def isPalindrome(word: str) -> bool:
     # Allows data transfer for stack variable
     global stack
     # Calculates word length
-    length = len(word)
+    wordLength = len(word)
     # Creates a stack according to word length
-    stack = ['0'] * (length +1)
+    stack = ['0'] * (wordLength +1)
     # Finds the middle of word
-    mid = length // 2
+    mid = wordLength // 2
     i = 0
     # Omits middle letter for odd number of letter words
     while i < mid:
-        push(word[i])
+        pushWord(word[i])
         i += 1
-    if length % 2 != 0:
+    if wordLength % 2 != 0:
         i += 1
     # Checks if letters are the same from front and back
-    while i < length:
-        character = pop()
+    while i < wordLength:
+        character = popWord()
         if character != word[i]:
             return False
         i += 1
     return True
 
 # Removes character from the top of stack
-def pop():
+def popWord():
     global top
     character = stack[top]
     top -= 1
     return character
 # Adds character to the top of stack
-def push(character: str):
+def pushWord(character: str):
     global top
     top += 1
     stack[top] = character
