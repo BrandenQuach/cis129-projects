f04bbd223e0bc39dd9621667e5e8a27224b5e5bf
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-05-01 20:22:20 -0700

    Update cis129_lab11_9.2.py

--- cis129_lab11_9.2.py
+++ cis129_lab11_9.2.py
@@ -1,1 +1,15 @@
+# Lab 11 9.2: Reading Grades
+# Author: Branden Quach
+# May 1, 2024
+# Reads grade.txt file
 
+# Opens grades.txt file in read mode
+with open('grades.txt', mode='r') as grades:
+    # Prints header for identification
+    print(f'{"Grade":<10}{"Total":<10}{"Count":<10}{"Average":<10}')
+
+    for record in grades:
+        # Splits text line
+        grade, total, count, average = record.split()
+        # Prints text
+        print(f'{grade:<10}{total:<10}{count:<10}{average:<10}')
