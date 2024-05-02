eff31cef627fa84f0c9ea7263de1261aaf4d504b
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-05-01 20:12:25 -0700

    Update cis129_lab11_9.1.py

--- cis129_lab11_9.1.py
+++ cis129_lab11_9.1.py
@@ -1,1 +1,24 @@
+# Lab 11 9.1: Grades to .txt
+# Author: Branden Quach
+# May 1, 2024
+# Collects grades with totals and average into a text file.
 
+# Opens text file in writing mode
+with open('grades.txt', mode='w') as grades:
+    # Variables
+    total = 0
+    count = 0
+    sentinel = -1
+    # While loop for multiple entries
+    while True:
+        # Prompt for grade input
+        grade = input(f'Enter a grade or {sentinel} to quit: ')
+        # Detects sentinel value to quit program
+        if grade == sentinel:
+            break
+        # Calculates total, count, average
+        total += int(grade)
+        count += 1
+        average = total/count
+        # prints data into text file
+        print(f'{grade} {total} {count} {average}', file=grades)
