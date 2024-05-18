62c23c92b6930ca009b2e14c0f1598ba20ac0736
BrandenQuach <158991144+BrandenQuach@users.noreply.github.com>
2024-05-17 20:16:16 -0700

    Update cis129_final.py

--- cis129_final.py
+++ cis129_final.py
@@ -2,15 +2,15 @@
 # Author: Branden Quach
 # May 16, 2024
 
-print(f'Answer for Question 1:')
+print(f'\nAnswer for Question 1:')
 total = 12.8 + 18.2 + 0.4
 print(total)
 
-print(f'Answer for Question 2:')
+print(f'\nAnswer for Question 2:')
 number = float(input(f'Enter a float number: '))
 print(f'{number} is a float.')
 
-print(f'Answer for Question 3:')
+print(f'\nAnswer for Question 3:')
 pi = 3.14159
 r = 3.5
 d = 2 * r
@@ -18,12 +18,12 @@ c = 2 * r * pi
 a = pi * (r ** 2)
 print(f'For a circle with a radius of {r}, the diameter is {d}, circumference is {c}, and area is {a}.')
 
-print(f'Answer for Question 4:')
+print(f'\nAnswer for Question 4:')
 print('number  square  cube')
 for number in range(0, 6):
     print('{0:3d}\t {1:3d}\t {2:3d}'.format(number, number * number, number * number * number))
 
-print(f'Answer for Question 5:')
+print(f'\nAnswer for Question 5:')
 y = 0
 z = 0
 x = int(input(f'Enter an integer'))
@@ -32,16 +32,16 @@ if x > 100:
     z = 40
 print(f'y is {y} and z is {z}')
 
-print(f'Answer for Question 6:')
+print(f'\nAnswer for Question 6:')
 list = [98, 76, 71, 87, 83, 90, 57, 79, 82, 94]
 average = sum(list)/len(list)
 print(f'The class average is {average}.')
 
-print(f'Answer for Question 7:')
+print(f'\nAnswer for Question 7:')
 for numbers in range(33, -1, -3):
     print(numbers, end=' ')
 
-print(f'\nAnswer for Question 8:')
+print(f'\n\nAnswer for Question 8:')
 myInt = 0
 def squared(myInt):
     myInt = int(input(f'Enter an integer to be squared.'))
@@ -50,7 +50,7 @@ def squared(myInt):
 squaredNumber =squared(myInt)
 print(squaredNumber)
 
-print(f'Answer for Question 9:')
+print(f'\nAnswer for Question 9:')
 from random import randint
 rolls = 10
 rollcount = 0
@@ -58,27 +58,27 @@ for amount in range(0, rolls):
     rollcount += 1
     print(f'Roll {rollcount}:',randint(1,6))
 
-print(f'Answer for Question 10:')
+print(f'\nAnswer for Question 10:')
 list1 = [10,20,30]
 list2 = [40,50]
 concatenated_list = list1 + list2
 print(concatenated_list)
 
-print(f'Answer for Question 11:')
+print(f'\nAnswer for Question 11:')
 x = 'ABC'
 x = '#'.join(x)
 print(x)
 
-print(f'Answer for Question 12:')
+print(f'\nAnswer for Question 12:')
 country_codes = {'Finland':'fi','South Africa':'za','Nepal':'np'}
 for key in country_codes.keys():
     print(key)
 
-print(f'Answer for Question 13:')
+print(f'\nAnswer for Question 13:')
 x = chr(66)
 print(f'{x}')
 
-print(f'Answer for Question 14:')
+print(f'\nAnswer for Question 14:')
 with open('accounts.txt', 'w') as file:
     file.write('100 Bill 12.34\n200 Joe 0.00\n300 Tom 4.21')
 
